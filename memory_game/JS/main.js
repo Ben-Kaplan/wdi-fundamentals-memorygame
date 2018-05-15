b98d2e40531df2d97e9a2056e522caf3d99f6f80
Benjamin Kaplan <benjaminkaplanart@gmail.com>
2018-05-14 20:34:50 -0600

    created game logic

---
+++ memory_game/JS/main.js
@@ -0,0 +1,12 @@
+console.log("Up and running!")
+
+var cardOne = "Queen";
+
+var carTwo = "King";
+
+var cardThree = "Queen";
+
+var cardFour ="King";
+
+console.log( "User flipped" + cardOne);
+console.log("User flipped" + cardThree);
